387dbd171c123b924b832d1d39824a5c22936a82
James Kim <yameskim@gmail.com>
2018-07-12 19:48:04 -0400

    Display Hello World

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-console.log('Hello World!');
+// console.log('Hello World!');
+
+var AppView = Backbone.View.extend({
+  el: '#container',
+  initialize: function() {
+    this.render();
+  },
+  render: function() {
+    console.log(this.el);
+    this.$el.html("Hello World!");
+  }
+});
+
+var appView = new AppView();
+console.log('Hello World');
