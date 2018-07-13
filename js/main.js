bda962bb71d9a035ba5ecb74dddb2ea039a5b77b
James Kim <yameskim@gmail.com>
2018-07-12 21:06:32 -0400

    Use underscore template

--- js/main.js
+++ js/main.js
@@ -1,17 +1,26 @@
 'use strict';
 
-// console.log('Hello World!');
-
 var AppView = Backbone.View.extend({
   el: '#container',
   initialize: function() {
     this.render();
   },
+  template: _.template("<h1>Hello <%= who %>!</h1>"),
   render: function() {
-    console.log(this.el);
-    this.$el.html("Hello World!");
+    this.$el.html(this.template({ who: "World"}));
   }
 });
+
+// el - Every View has an element that is basically
+// an injection point.
+
+// inititialize - First function that executes
+// in the view when it's instantiated.
+
+// _.template is so we can dynamically insert data:
+// <%= %> escapes the HTML
+// <%- %> doesn't escape the HTML (for stuff like partials: include();)
+// <% %> to use JS
 
 var appView = new AppView();
 console.log('Hello World');
